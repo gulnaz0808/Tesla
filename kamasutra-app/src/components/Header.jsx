0c98e7eb5c48e277da8ad785372d07ee1a7079bd
Gigi bekmatova <gigibekmatova@mail.ru>
2023-11-01 16:56:51 -0700

    Initial commit

---
+++ kamasutra-app/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='header'>
+        <img src='https://th.bing.com/th/id/OIP.6RZes4QH4yUImQSlFtOdjQHaEM?pid=ImgDet&rs=1'/>
+      </header>
+  )
+}
+
+export default Header;
---
+++ kamasutra-app/src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+      <img src='https://th.bing.com/th/id/OIP.-33GfPL47prhjycT9CsB3gHaE8?pid=ImgDet&rs=1'/>
+      </div>
+      <div>
+        ava + description
+      </div>
+      <div>
+        My post
+        <div>
+          New post
+        </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
